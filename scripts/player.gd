7cf08581ba68dcfccc011c54024f9f941eff6f4f
Mark Washeim <blueprint@poetaster.de>
2023-01-30 08:28:44 +0100

    [fix] class was mising

---
+++ scripts/player.gd
@@ -0,0 +1,13 @@
+# player state class
+extends Resource
+
+class_name Player
+
+export var player_name = ""
+export var wins = 0
+export var played = 0
+export var high_score = 0
+#export var high_score = {
+#		"value": 0,
+#		"date": ""
+#	}
